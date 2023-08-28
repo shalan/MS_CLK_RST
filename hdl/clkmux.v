9cb2fceeed0b9748dcbf006bceab0fffe8d5b386
Mohamed Shalan <mshalan@aucegypt.edu>
2023-08-28 17:39:50 +0300

    changed port names

--- hdl/clkmux.v
+++ hdl/clkmux.v
@@ -27,8 +27,8 @@
 
 module clkmux_2x1 (
     input   wire    rst_n,
-    input   wire    clk1, 
-    input   wire    clk2,
+    input   wire    clk0, 
+    input   wire    clk1,
     input   wire    sel,
     output  wire    clko
 );
@@ -36,7 +36,7 @@ module clkmux_2x1 (
     reg Q1a, Q1b, Q2a, Q2b;
     wire q1a_in, q2a_in;
     
-    assign clko = (clk1 & Q1b) | (clk2 & Q2b);
+    assign clko = (clk0 & Q1b) | (clk1 & Q2b);
     
     wire  Q2b_bar = ~Q2b;
     wire  Q1b_bar = ~Q1b;
@@ -45,25 +45,25 @@ module clkmux_2x1 (
     assign q1a_in = Q2b_bar & sel_bar;
     assign q2a_in = Q1b_bar & sel;
     
-    always @(posedge clk1 or negedge rst_n) 
+    always @(posedge clk0 or negedge rst_n) 
         if (~rst_n) 
             Q1a <= 1'b0; 
         else 
             Q1a <= q1a_in;
 
-    always @(negedge clk1 or negedge rst_n) 
+    always @(negedge clk0 or negedge rst_n) 
         if (~rst_n) 
             Q1b <= 1'b0; 
         else 
             Q1b <= Q1a;
 
-    always @(posedge clk2 or negedge rst_n) 
+    always @(posedge clk1 or negedge rst_n) 
         if (~rst_n) 
             Q2a <= 1'b0; 
         else 
             Q2a <= q2a_in;
 
-    always @(negedge clk2 or negedge rst_n) 
+    always @(negedge clk1 or negedge rst_n) 
         if (~rst_n) 
             Q2b <= 1'b0; 
         else 
@@ -73,10 +73,10 @@ endmodule
 
 module clkmux_4x1 (
     input   wire        rst_n,
+    input   wire        clk0, 
     input   wire        clk1, 
     input   wire        clk2, 
-    input   wire        clk3, 
-    input   wire        clk4,
+    input   wire        clk3,
     input   wire [1:0]  sel,
     output  wire        clko
 );
@@ -85,22 +85,22 @@ module clkmux_4x1 (
 
     clkmux_2x1 m1(  
                     .rst_n(rst_n),
+                    .clk0(clk0), 
                     .clk1(clk1), 
-                    .clk2(clk2), 
                     .clko(clko1), 
                     .sel(sel[0])
                 );
     clkmux_2x1 m2(  
                     .rst_n(rst_n),
+                    .clk0(clk2), 
                     .clk1(clk3), 
-                    .clk2(clk4), 
                     .clko(clko2), 
                     .sel(sel[0])
                 );
     clkmux_2x1 m3(  
                     .rst_n(rst_n),
-                    .clk1(clko1), 
-                    .clk2(clko2), 
+                    .clk0(clko1), 
+                    .clk1(clko2), 
                     .clko(clko), 
                     .sel(sel[1])
                 );
